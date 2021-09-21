efa9d182ebbd3e8bc665e20d8d986287c18d71f3
Mohammad9760 <sensimdh81@gmail.com>
2021-09-21 08:45:01 +0430

    heli changed
    changed heli back.
    and removed some unneccesary files.

--- FreedomTrail/Scripts/HeliMovement.cs
+++ FreedomTrail/Scripts/HeliMovement.cs
@@ -11,14 +11,14 @@ public class HeliMovement : MonoBehaviour
 
     private void Start()
     {
-       // offset = transform.position - target.position;
-        offset = new Vector3(0,height,0);
+        offset = transform.position - target.position;
+       // offset = new Vector3(0,height,0);
     }
 
     private void Update()
     {
 
-        transform.position = Vector3.Lerp(transform.position, target.position + offset+ (target.forward * -distance), Time.deltaTime* speedMove);
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime* speedMove);
         
         transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(
             transform.forward, target.position - transform.position, speedRotate * Time.deltaTime, 0.0f
