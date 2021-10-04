c6cf82199b6aa9aa517ab73054ef8784c66f4137
Mehrab <aghaeemehrab@gmail.com>
2021-10-04 14:37:26 +0330

    aa

--- FreedomTrail/Scripts/EnemyCarTargetController.cs
+++ FreedomTrail/Scripts/EnemyCarTargetController.cs
@@ -6,6 +6,8 @@ public class EnemyCarTargetController : MonoBehaviour
 {
 
     private CarAIControl car;
+    private Transform playerTransform, target;
+    //private float stoppingDist = 8;
 
     private void Start()
     {
@@ -23,10 +25,19 @@ public class EnemyCarTargetController : MonoBehaviour
         */
         
         car.m_Target = GameObject.Find("Player Car").transform;
+        //playerTransform = GameObject.Find("Player Car").transform;
+        //target = car.m_Target;
     }
 
-    void Update()
+    private void Update()
     {
+
+       // print("direction" + ((transform.position - playerTransform.position).normalized.z > 0? 1: -1));
+
+      //  target.position = (transform.position + (transform.position - playerTransform.position).normalized) * Vector3.Distance(transform.position, playerTransform.position);
+
+        //car.m_Target = target;
+        //car.m_Driving = (Vector3.Distance(transform.position, playerTransform.position) > stoppingDist);
         
     }
 }
